0d4d3d6a9d2994c47643aa85271df630f46fa45d
EduardoMG12 <charles.eduardo.mello.guimaraes@hotmail.com>
2023-01-25 15:11:58 -0300

    feat(): added component Loading and logic, and refactoring for context use

--- src/pages/Card/index.tsx
+++ src/pages/Card/index.tsx
@@ -1,11 +1,13 @@
 import { Container } from '@mui/system'
 import axios from 'axios'
-import React, { useEffect, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom';
-import { IPokemons } from '../Home';
+import Loading from '../../components/Loading';
+import { IPokemons } from '../../components/Main';
+import { PokemonsContext } from '../../contexts/PokemonsContext';
 
 const Card = () => {
-    const [pokemon, setPokemon] = useState<IPokemons>()
+    const [pokemon, setPokemon] = useState<IPokemons[]>([])
     const { id } = useParams()
 
     useEffect(() => {
@@ -18,9 +20,20 @@ const Card = () => {
     }, [])
 
     return (
-        <Container>
-            <img src={pokemon?.sprites.front_default} alt="" />
-        </Container>
+        <>
+            {pokemon.length == 0
+                ?
+
+                < Loading />
+
+                :
+
+                <Container>
+                    <img src={pokemon?.sprites.front_default} alt="" />
+                </Container>
+
+            }
+        </>
     )
 }
 
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,57 +1,39 @@
 import { Container, Grid } from '@mui/material'
-import axios from 'axios'
-import React, { useEffect, useState } from 'react'
+import React, { useContext } from 'react'
 import PokemonCard from '../../components/PokemonCard'
 import { Section } from './style'
 import Button from '../../components/Button'
 import { Link } from 'react-router-dom'
-
-export interface IPokemons {
-    id?: number,
-    name: string,
-    image: string,
-    alt: string,
-    sprites?: any
-}
+import Loading from '../../components/Loading'
+import { IPokemons } from '../../components/Main'
+import { PokemonsContext } from '../../contexts/PokemonsContext'
 
 export const Home = () => {
-    const [pokemons, setPokemons] = useState<IPokemons[]>([]);
+    const { pokemons, setPokemons, getPokemons } = useContext(PokemonsContext)
 
-    useEffect(() => {
-        getPokemons(20)
-    }, [])
 
-    const getPokemons = (pokemonsQuantity: number) => {
-        let endPoints = []
-        let cardsLoading = pokemonsQuantity || 10;
-
-        for (let i = 1; i <= cardsLoading; i++) {
-            endPoints.push(`https://pokeapi.co/api/v2/pokemon/${i}`)
-        }
-        const response = axios.all(endPoints.map((url: string) => axios.get(url))).then((res) => {
-            setPokemons(res.map((r: any) => r.data))
-        }).catch((res: string) => console.log(res))
-
-        return response;
-    }
-    console.log(pokemons)
     return (
         <Section>
             <Container maxWidth={false}>
-                <Grid container spacing={3} gridTemplateColumns={"repeat(auto-fill, minmax(345px, 1fr)"}>
-                    {pokemons.map((pokemon) => {
-                        return (
-                            <Grid item key={pokemon.id}>
-                                <Link to={`/card/${pokemon.id}`}>
-                                    <PokemonCard name={pokemon.name} image={pokemon.sprites.front_default || ""} alt={`image of ${pokemon.name}`} />
-                                </Link>
-                            </Grid>
-                        )
-                    })}
-                </Grid>
-
+                {
+                    pokemons.length === 0
+                        ?
+                        <Loading />
+                        :
+                        <Grid container spacing={3} gridTemplateColumns={"repeat(auto-fill, minmax(345px, 1fr)"}>
+                            {pokemons.map((pokemon: any) => {
+                                return (
+                                    <Grid item key={pokemon.id}>
+                                        <Link to={`/card/${pokemon.id}`}>
+                                            <PokemonCard name={pokemon.name} image={pokemon.sprites.front_default || ""} alt={`image of ${pokemon.name}`} />
+                                        </Link>
+                                    </Grid>
+                                )
+                            })}
+                        </Grid>
+                }
             </Container>
-            <Button label={"Loading more"} onClick={() => { getPokemons(pokemons.length + 5) }} />
+            <Button label={"Loading more"} onClick={() => { getPokemons(pokemons.length + 33) }} />
         </Section>
     )
 }
