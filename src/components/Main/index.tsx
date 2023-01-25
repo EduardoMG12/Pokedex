c5386ee57f91f3594a76d931c51da586ecdd81b7
EduardoMG12 <charles.eduardo.mello.guimaraes@hotmail.com>
2023-01-25 15:10:24 -0300

    feat(): implements context at component main

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,14 +1,45 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 import { Outlet } from 'react-router-dom'
+import { PokemonsContext } from '../../contexts/PokemonsContext'
 import Footer from '../Footer'
 import Header from '../NavBar'
 
+export interface IPokemons {
+    id?: number,
+    name: string,
+    image: string,
+    alt: string,
+    sprites?: any
+}
+
 export default () => {
+    const [pokemons, setPokemons] = useState<IPokemons[]>([]);
+
+    useEffect(() => {
+        getPokemons()
+    }, [])
+
+    const getPokemons = (pokemonsQuantity?: number) => {
+        let endPoints = []
+        let cardsLoading = pokemonsQuantity || 33;
+
+        for (let i = 1; i <= cardsLoading; i++) {
+            endPoints.push(`https://pokeapi.co/api/v2/pokemon/${i}`)
+        }
+        const response = axios.all(endPoints.map((url: string) => axios.get(url))).then((res) => {
+            setPokemons(res.map((r: any) => r.data))
+        }).catch((res: string) => console.log(res))
+
+        return response;
+    }
     return (
-        <>
+
+        <PokemonsContext.Provider value={{ pokemons, setPokemons, getPokemons }}>
             <Header />
             <Outlet />
             <Footer />
-        </>
+        </PokemonsContext.Provider>
+
     )
 }
