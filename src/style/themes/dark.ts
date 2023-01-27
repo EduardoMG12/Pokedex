a7bebddc137c3f42763910c2cb7293b60776347a
EduardoMG12 <charles.eduardo.mello.guimaraes@hotmail.com>
2023-01-27 10:56:49 -0300

    feat(): added folder themes with light and dark files for using themeProvider

---
+++ src/style/themes/dark.ts
@@ -0,0 +1,10 @@
+export default {
+    title: 'dark',
+
+    colors: {
+        backgroundColors: "rgba(0, 0, 0, 0.9)", //Header, Footer
+        text: "rgba(221,217,206, 0.9)",
+    },
+
+    fonts: {}
+}
