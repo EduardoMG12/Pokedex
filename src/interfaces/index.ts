eeb4924be58af498f4025c800eb8e9a29dd5bd51
EduardoMG12 <charles.eduardo.mello.guimaraes@hotmail.com>
2023-01-27 10:11:53 -0300

    feat(): added more elements in sprites in the interface for the new resource in the card page

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -3,5 +3,10 @@ export interface IPokemon {
     name: string,
     image: string,
     alt: string,
-    sprites?: { front_default: string }
+    sprites?: {
+        front_default: string,
+        back_default: string,
+        front_shiny: string,
+        back_shiny: string
+    }
 };
